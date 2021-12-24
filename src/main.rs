eb6d6f2f9717673cbe38ac1aab537fa85f69aecf
Pavel Kasila <pavel.kasila@gmail.com>
2021-12-24 09:47:40 +0300

    Add rendering using ffmpeg subprocess

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::io::Read;
+use std::process::{Command, Stdio};
 use corevox::network::client::{VoxClient, VoxClientImpl};
 use corevox::network::messages::VoxPack;
 
@@ -8,9 +10,26 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut client = VoxClientImpl::new("0.0.0.0:1990".to_string()).await.unwrap();
 
+    let process = match Command::new("/bin/sh")
+        .arg("-c")
+        .arg(format!("ffmpeg -i {} -s {}x{} -r 7 -t 1 -pix_fmt rgb565le -f rawvideo pipe:",
+                     std::env::args().nth(1).unwrap(),
+                     client.device_info.frame_size[0], client.device_info.frame_size[1]))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn() {
+        Err(why) => panic!("couldn't spawn ffmpeg: {}", why),
+        Ok(process) => process,
+    };
+
+    let mut s: Vec<u8> = vec![0; (client.device_info.frame_size[0] * client.device_info.frame_size[1] * 7 * 2) as usize];
+    process.stdout.unwrap().read_exact(&mut s).unwrap_or_else(|e| { panic!("couldn't read ffmpeg stdout: {}", e) });
+
+    println!("File size: {}", s.capacity());
+
     client.send_pack(VoxPack {
         z: 7,
-        raw: vec![0x00, 0x01, 0x02, 0x03]
+        raw: s,
     }).await.unwrap();
 
     return Ok(());
