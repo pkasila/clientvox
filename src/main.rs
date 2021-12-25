1d7b4efaaacfbaa5c05b1b926e3d686bbc6fec31
Pavel Kasila <pavel.kasila@gmail.com>
2021-12-25 12:21:04 +0300

    Use H264 encoding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::io::Read;
+use std::io::{Read, BufRead};
 use std::process::{Command, Stdio};
 use corevox::network::client::{VoxClient, VoxClientImpl};
 use corevox::network::messages::VoxPack;
@@ -12,7 +12,7 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 
     let process = match Command::new("/bin/sh")
         .arg("-c")
-        .arg(format!("ffmpeg -i {} -s {}x{} -r 7 -t 1 -pix_fmt rgb565le -f rawvideo pipe:",
+        .arg(format!("ffmpeg -i {} -s {}x{} -r 7 -t 1 -c:v h264_videotoolbox -f mp4 -movflags frag_keyframe+empty_moov pipe:",
                      std::env::args().nth(2).unwrap(),
                      client.device_info.frame_size[0], client.device_info.frame_size[1]))
         .stdin(Stdio::piped())
@@ -22,10 +22,10 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
         Ok(process) => process,
     };
 
-    let mut s: Vec<u8> = vec![0; (client.device_info.frame_size[0] * client.device_info.frame_size[1] * 7 * 2) as usize];
-    process.stdout.unwrap().read_exact(&mut s).unwrap_or_else(|e| { panic!("couldn't read ffmpeg stdout: {}", e) });
+    let mut s: Vec<u8> = vec![];
+    process.stdout.unwrap().read_to_end(&mut s).unwrap_or_else(|e| { panic!("couldn't read ffmpeg stdout: {}", e) });
 
-    println!("File size: {}", s.capacity());
+    println!("File size: {}", s.len());
 
     client.send_pack(VoxPack {
         z: 7,
